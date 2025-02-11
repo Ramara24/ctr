f0c1eb1480a106d72cee3cfd664b998f87e2d4c7
Ramara24 <reem243@hotmail.com>
2025-02-11 02:12:13 +0200

    Update fill_na_preprocessor.py

--- preprocessor/fill_na_preprocessor.py
+++ preprocessor/fill_na_preprocessor.py
@@ -47,11 +47,28 @@ class FillNaPreprocessor(BasePreprocessor):
             self.fill_values = {col: self.fill_method(X[col]) for col in X.columns}
         return self
 
-    def transform(self, X: pd.DataFrame, y=None):
-        if self.fill_values is None:
-            raise ValueError("FillNaPreprocessor should have been fit before transform")
-        return X[self.columns].fillna(value=self.fill_values) if self.columns else X.fillna(value=self.fill_values)
+    #def transform(self, X: pd.DataFrame, y=None):
+    #  if self.fill_values is None:
+    #      raise ValueError("FillNaPreprocessor should have been fit before transform")
+    # return X[self.columns].fillna(value=self.fill_values) if self.columns else X.fillna(value=self.fill_values)
 
+    def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
+            if self.fill_values is None:
+                raise ValueError("Fit the preprocessor first!")
+            
+            # Fill NaNs without dropping other columns
+            X_filled = X.copy()
+            if self.columns:
+                for col in self.columns:
+                    X_filled[col] = X_filled[col].fillna(self.fill_values[col])
+                    if X_filled[col].dtype == 'float64':  # Convert float categoricals to strings
+                        X_filled[col] = X_filled[col].astype(str)
+            else:
+                for col in X_filled.columns:
+                    X_filled[col] = X_filled[col].fillna(self.fill_values[col])
+                    if X_filled[col].dtype == 'float64':  # Convert float categoricals to strings
+                        X_filled[col] = X_filled[col].astype(str)
+            return X_filled
 
 if __name__ == "__main__":
     loader = TrainLoaderSessionSplitter(train_file=getroot() + "/data/train_dataset_full.csv")
