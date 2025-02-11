e655f5c8912dfe5ac4ad02cfdc468c0cc5db2e52
Ramara24 <reem243@hotmail.com>
2025-02-11 21:12:24 +0200

    Update pickle_object.py

--- pickle_object.py
+++ pickle_object.py
@@ -12,14 +12,14 @@ class PickleObject:
         Save the fitted preprocessor to a pickle file.
         """
         file_path = os.path.join(os.getenv('PROJECT_ROOT'), 'results', self.result_path,
-                                 f'{class_name or self.__class__.__name__}-{datetime.now().strftime('%Y%m%d_%H%M%S')}.pkl')
+                                 f"{class_name or self.__class__.__name__}-{datetime.now().strftime('%Y%m%d_%H%M%S')}.pkl")
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "wb") as f:
             pickle.dump(self, f)
 
     def dump_results(self, class_name: str = None, results: str = ''):
         file_path = os.path.join(os.getenv('PROJECT_ROOT'), 'results', self.result_path,
-                                 f'CV-{class_name or self.__class__.__name__}-{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt')
+                                 f"CV-{class_name or self.__class__.__name__}-{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt")
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w") as f:
             f.write(results)
